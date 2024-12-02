03e3e721743a7c201a0287556b26790c05c977b1
Timur Makarov <public@timurmakarov.com>
2024-12-02 13:45:10 +0400

    fix: return error from 'OpenDB' function

--- config/prerun.go
+++ config/prerun.go
@@ -31,12 +31,16 @@ func OpenDB(dsn string) (*gorm.DB, error) {
 	var db *gorm.DB
 	var err error
 
-	for i := 0; i < 5; i++ {
+	for i := 1; i <= 5; i++ {
 		db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+		if err != nil && i == 5 {
+			return nil, err
+		}
+
 		if err != nil {
-			log.Printf("Could not connect to DB. Retry #%d\n", i)
+			log.Printf("Could not connect to DB. Retry #%d \n", i)
 			time.Sleep(1 * time.Second)
-			continue
 		}
 	}
 
